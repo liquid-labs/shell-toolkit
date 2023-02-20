ccf02e18e73e5df29f5bb7afa48f9667c8a826f9
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-20 16:19:30 -0600

    added support for 'noThrow' option and maxed test coverage

--- src/test/try-exec.test.js
+++ src/test/try-exec.test.js
@@ -9,5 +9,17 @@ describe('tryExec', () => {
     expect(result.stdout.trim()).toEqual('foo')
   })
 
-  test('repeats command on error', () => expect(() => tryExec('blahblahblah')).toThrow(/blahblahblah/))
+  test("uses 'msg' in as excepton message", () =>
+    expect(() => tryExec('blahblahblah', { msg: 'what' })).toThrow(/what/))
+
+  test('makes result available to msgFunc', () => 
+    expect(() => tryExec('blahblahblah', { msgFunc: (result) => `Exit status: ${result.code}` }))
+      .toThrow(/Exit status: \d+/))
+
+  test('reflects command in exception message', () => expect(() => tryExec('blahblahblah')).toThrow(/blahblahblah/))
+
+  test("'noThrow=true' supresses the throw on error behavior", () => {
+    const result = tryExec('blahblahblah', { noThrow: true })
+    expect(result.code).not.toBe(0)
+  })
 })
--- src/try-exec.mjs
+++ src/try-exec.mjs
@@ -1,12 +1,12 @@
 import createError from 'http-errors'
 import shell from 'shelljs'
 
-const tryExec = (cmd, { httpStatus = 500, msg = '', msgFunc } = {}) => {
+const tryExec = (cmd, { httpStatus = 500, msg = '', msgFunc, noThrow = false } = {}) => {
   const result = shell.exec(cmd, { silent : true })
-  if (result.code !== 0) {
+  if (result.code !== 0 && noThrow !== true) {
     if (msg.length > 0) msg += ' '
     if (msgFunc !== undefined) msg += msgFunc(result) + ' '
-
+    
     throw createError(httpStatus, msg + `Failed to execute '${cmd}'; stderr: ${result.stderr}`)
   }
 

1480b4c40c84d000fe8fadf0a9b5e53ffee0a08f
Zane Rockenbaugh <zane@liquid-labs.com>
2023-07-19 13:57:44 -0500

    improved error message

--- src/lib/error-msg.mjs
+++ src/lib/error-msg.mjs
@@ -1,7 +1,7 @@
 const errorMsg = ({ cmd, msg = '', msgFunc, result, stderr }) => {
   if (msg.length > 0) msg += ' '
   if (msgFunc !== undefined) msg += msgFunc(result) + ' '
-  msg += `Failed to execute '${cmd}'; stderr: ${result.stderr}`
+  msg += `Failed to execute '${cmd}' (${result.code}); stderr: ${result.stderr}; stdout: ${result.stdout}`
 
   return msg
 }

3d1216cdcccee94709f1bee0643c082a9be67cdd
Zane Rockenbaugh <zane@liquid-labs.com>
2024-01-01 17:17:47 -0600

    fix noThrow setting

--- src/try-exec.mjs
+++ src/try-exec.mjs
@@ -2,7 +2,7 @@ import shell from 'shelljs'
 
 import { errorMsg } from './lib/error-msg'
 
-const tryExec = (cmd, { noThrow = true, silent = true, ...opts } = {}) => {
+const tryExec = (cmd, { noThrow = false, silent = true, ...opts } = {}) => {
   const result = shell.exec(cmd, { silent, ...opts })
   if (result.code !== 0 && noThrow !== true) {
     throw new Error(errorMsg({ cmd, result, ...opts }))
